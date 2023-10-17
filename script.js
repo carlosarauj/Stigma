771ecde50294d5b2cf733954d766942e75052939
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-10-16 21:20:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+/*
+⠀⠀   ⢀⡴⠁⠀ ⣿⡏⠀⠱⣄
+⠀⠀⢀⣴⡟⠁⠀⠀⠀⣿⡇⠀⠀⠀⠙⣷⣄
+⠀⠀⠙⢿⣷⣄⠀⠀⠀⣿⡇⠀⠀⢀⣴⣿⠋
+⠀⠀⠀⠀⠙⢿⣷⣄⠀⢻⡇⢀⣴⣿⠋
+⠀⠀⠀⠀⠀⠀⠈⠻⣷⣾⣷⡿⠋
+⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣷⣄
+⠀⠀⠀⠀⠀⢀⣶⣿⠟⢹⣏⠻⢿⣷⣄
+⠀⠀⠀⢀⣼⣿⠟⠁⠀⢸⣿⠀⠈⠙⢿⣷⣄
+⠀⠀⣴⣿⡟⠁⠀⠀⠀⢸⣿⠀⠀⠀⠀⣹⣿⡷
+⠀⠀⠈⠻⣿⣦⡀⠀⠀⢸⣿⠀⠀⢀⣼⣿⠏
+⠀⠀⠀⠀⠈⠻⣿⣦⡀⢸⣿⠀⣴⣿⠟⠁
+⠀⠀⠀⠀⠀⠀⠈⠻⣿⣾⣿⣾⡿⠃⠀
+⠀⠀ ⠀⠀⠀⠀⠀⠀⠈⠻⡿⠋
+"My honored brethen
+We come together
+To unite as one
+Against those that are damned
+We show no mercy
+For we have none
+Our enemies shall fall
+As we uprise
+To claim our fate
+Now and forever
+We'll be together
+Love and hate"
+*/
